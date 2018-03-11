034b04a764321574e4694920e14d709a02a37970
zx11161988 <zx1116_1988@163.com>
2018-03-11 23:53:09 +0800

    Update 1-headers.py

--- chapter12/1-headers.py
+++ chapter12/1-headers.py
@@ -2,9 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 
 session = requests.Session()
-headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit 537.36 (KHTML, like Gecko) Chrome","Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"}
-url = "https://www.whatismybrowser.com/detect/what-http-headers-is-my-browser-sending"
+headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.146 Safari/537.36"}
+url = "https://cd.lianjia.com/chengjiao/pg2c3011056131816/?sug=%E5%8D%8E%E9%98%B3%E6%BB%A8%E6%B2%B3%E8%8A%B1%E5%9B%AD"
 req = session.get(url, headers=headers)
-
+#print(req.text)
 bsObj = BeautifulSoup(req.text, "html.parser")
-print(bsObj.find("table",{"class":"table-striped"}).get_text)
+print(bsObj.find("div", class_="page-box house-lst-page-box").attrs["page-data"])
