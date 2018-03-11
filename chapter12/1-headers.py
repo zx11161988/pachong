29f255b7f8fdde9dff3003de78c1b56f7e5bf92d
zx11161988 <zx1116_1988@163.com>
2018-03-12 00:09:47 +0800

    Update 1-headers.py
    url = "https://cd.lianjia.com/chengjiao/c3011056131816/?sug=滨河花园"

--- chapter12/1-headers.py
+++ chapter12/1-headers.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 session = requests.Session()
 headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.146 Safari/537.36"}
 #文路径转换：https://www.cnblogs.com/hannover/p/4657463.html#
-url = "https://cd.lianjia.com/chengjiao/pg2c3011056131816/?sug=%E5%8D%8E%E9%98%B3%E6%BB%A8%E6%B2%B3%E8%8A%B1%E5%9B%AD"
+url = "https://cd.lianjia.com/chengjiao/c3011056131816/?sug=滨河花园"
 req = session.get(url, headers=headers)
 #print(req.text)
 bsObj = BeautifulSoup(req.text, "html.parser")
